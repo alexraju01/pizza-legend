2e016cb086271e6bb859d009d967e0c2ffc07ea8
alexraju01 <alex.raju1010@gmail.com>
2026-04-16 13:03:02 +0100

    Initial setup

---
+++ Overworld.js
@@ -0,0 +1,35 @@
+class Overworld {
+	constructor(config) {
+		this.element = config.element;
+		this.canvas = this.element.querySelector(".game-canvas");
+		this.ctx = this.canvas.getContext("2d");
+	}
+
+	init() {
+		console.log("Overworld is ready!", this);
+		const x = 1;
+		const y = 4;
+
+		const image = new Image();
+		image.onload = () => {
+			this.ctx.drawImage(image, 0, 0);
+		};
+		image.src = "/images/maps/DemoLower.png";
+
+		const hero = new Image();
+		hero.onload = () => {
+			this.ctx.drawImage(
+				hero,
+				0, //left cut
+				0, //top cut,
+				32, //width of cut
+				32, //height of cut
+				x * 16 - 8,
+				y * 16 - 18,
+				32,
+				32,
+			);
+		};
+		hero.src = "/images/characters/people/hero.png";
+	}
+}
